4da48f9b9fa3f6c5f8037bda7e010d2d39d9132f
russa <aaron.russ@dfki.de>
2017-08-07 16:33:25 +0200

    update to use mmir-lib v4

--- www-example/app.js
+++ www-example/app.js
@@ -1,38 +1,38 @@
-var IS_DEBUG_ENABLED = false;	
+var IS_DEBUG_ENABLED = false;
 
 mmir.ready(function () {
-	
+
 	test_isNetworkAvailable();
-	
+
 	//prepare app-module object for public export of functions etc.
 	mmir.app = {};
-	
+
     //initialize BACK-BUTTON handling:
 	initHistoryBackHandler();
-	
-    //start app by triggering INIT event on dialog-engine:
-    mmir.DialogManager.raise('init');
 
-    
+    //start app by triggering INIT event on dialog-engine:
+    mmir.dialog.raise('init');
+
+
     //setup handler for BACK button (and for swipe-left gesture)
     function initHistoryBackHandler() {
-    	
-    	var isCordovaEnv = ! mmir.Constants.isBrowserEnv();
-    	
+
+    	var isCordovaEnv = mmir.const.isCordovaEnv();
+
     	//generic BACK handler:
     	var backButtonHandler = function (event){
-        	
+
         	if(isCordovaEnv || ( ! isCordovaEnv && event.state)){
-        		
+
         		//FIX for browser-env.: to popstate-event is triggered not only when back-button is pressed in browser (however, in this case it seems, that the event.state is empty...)
         		if( ! isCordovaEnv && ! event.state){
         			event.preventDefault();
         			return false; /////////////////////////// EARLY EXIT ///////////////////////////////////
         		}
-        		
+
         		//let state-machine handle BACK event
         		// -> see config/statedef/dialogDescriptionSCXML.xml transition for back-event
-        		mmir.DialogManager.raise('back', {
+        		mmir.dialog.raise('back', {
     			    nativeBackButton : 'true'
     			});
         	}
@@ -41,9 +41,9 @@ mmir.ready(function () {
         		return false;
         	}
         };
-        
+
         //register BACK-handler for environments and gesture:
-        
+
         if(isCordovaEnv){
 	    	//overwrite BACK-event for Android/cordova environment:
 	        document.addEventListener("backbutton", backButtonHandler, true);
@@ -52,25 +52,21 @@ mmir.ready(function () {
     	    //overwrite BACK-event in Browser environment:
     		window.addEventListener("popstate", backButtonHandler, true);
         }
-        
+
     }
-    
+
     ///////////////////////////////////////// TEST and DEBUG functions ////////////////////////////////////////
-        
+
     function test_isNetworkAvailable(){
-    	
+
     	// Check if a network connection is established.
-    	if (mmir.CommonUtils.checkNetworkConnection() == false){
+    	if (mmir.util.checkNetworkConnection() == false){
     		alert("No network connection enabled.\nPlease enable network access.");
     	} else {
     		console.log("Network access is available.");
     	}
-    	
+
     };
-    
-    
+
+
 });
-
-
-
-
